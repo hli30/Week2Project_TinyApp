b77a416fd02553f109014b4af9da03ce8fc90157
hli30 <harrylll93@gmail.com>
2018-02-20 22:07:09 +0000

    Added new route to handle posts to /urls

--- tinyAppServer.js
+++ tinyAppServer.js
@@ -21,6 +21,15 @@ app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
