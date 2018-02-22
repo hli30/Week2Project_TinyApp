408ade5f512bdabfec9c74beb69b4ae9ad147391
hli30 <harrylll93@gmail.com>
2018-02-22 00:24:58 +0000

    Added comments

--- tinyAppServer.js
+++ tinyAppServer.js
@@ -18,7 +18,10 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-//Handle GET Requests
+/*****
+ * Handle Get Requests
+ *****/
+//Route: home page
 app.get("/urls", (req, res) => {
   let templateVars = { 
     urls: urlDatabase,
@@ -27,11 +30,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//Route: create new page
 app.get("/urls/new", (req, res) => {
   let templateVars = { username: req.cookies["username"] }
   res.render("urls_new", templateVars);
 });
 
+//Route: detail + update page
 app.get("/urls/:id", (req, res) => {
   let templateVars = { 
     shortURL: req.params.id,
@@ -41,33 +46,41 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//Redirect: to actual page (longURL)
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-//Handle POST Requests
+/******
+ * Handle POST Requests
+ ******/
+//Assigns random id to longURL
 app.post("/urls", (req, res) => {
   let uid = generateRandomString();
   urlDatabase[uid] = req.body.longURL;
   res.redirect(`http://localhost:8080/urls/${uid}`);
 });
 
+//Updates the longURL in database
 app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls");
 })
 
+//Delete url from database
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 })
 
+//Login and set cookie
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/urls");
 })
 
+//Logout and "removes" cookie (sets it to undefined)
 app.post("/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect("/urls");
